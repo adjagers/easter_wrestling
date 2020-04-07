6b8cfae1277b25325dfa0733ed0d453163d5ddd6
Anton Jagers <adjagers13@gmail.com>
2020-04-07 09:04:40 +0200

    Matches page
    adding styling

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       filename: 'matches.html',
-      template: '../src/matches.html',
+      template: 'src/matches.html',
     }),
   ],
 }
